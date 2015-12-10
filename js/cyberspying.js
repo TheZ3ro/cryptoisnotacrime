0b2a7d89629d860105d86b3dbe55d0b1d44e1b6d
thez3ro <silvethebest@yahoo.it>
2015-12-10 14:09:59 +0100

    first

---
+++ js/cyberspying.js
@@ -0,0 +1,60 @@
+$(function(){
+  // twitter names
+  var committee = ['Clyburn', 'RepBecerra', 'RepDerekKilmer', 'DennyHeck', 'MarkPocan', 'MarcVeasey', 'FilemonVela', 'petegallego', 'Castro4Congress', 'BetoORourke', 'CartwrightPA', 'JoyceBeatty', 'DanMaffeiNY', 'spmaloney', 'RepJeffries', 'RepGraceMeng', 'Michelle4NM', 'RepAnnieKuster', 'TeamSheaPorter', 'RepCheri', 'RepBillFoster', 'JoeGarcia', 'RepLoisFrankel', 'RepLowenthal', 'RepMarkTakano', 'CongressmanRuiz', 'JuliaBrownley', 'RepSwalwell', 'RepBera', 'RepHuffman', 'kyrstensinema', 'RepAnnaEshoo', 'RepThompson', 'janschakowsky', 'jimlangevin', 'RepAdamSchiff', 'LuisGutierrez', 'jahimes', 'RepTerriSewell'];
+
+  // random tweets
+  var rep_tweets = [
+    'Oppose CISPA, @[[twitter_name]]! Preserve privacy and liberty on the Internet. https://eff.org/CISPA #CISPAalert',
+    'Hey @[[twitter_name]]: CISPA sacrifices liberty without improving security.  We deserve both. https://eff.org/CISPA #CISPAalert',
+    'Hey @[[twitter_name]]: We can\'t have security without privacy. Help us defend the Internet from CISPA! https://eff.org/CISPA #CISPAalert',
+    'Hey @[[twitter_name]], a free and open internet depends on more privacy but CISPA gives us less. Vote no! https://eff.org/CISPA #CISPAalert',
+    'Hey @[[twitter_name]]: CISPA allows our information to go to agencies who don\'t care about our privacy. https://eff.org/CISPA #CISPAalert',
+  ];
+  var leader_tweets = [
+    'Hey @WhipHoyer @NancyPelosi: CISPA sacrifices liberty without improving security. We deserve both. https://eff.org/CISPA #CISPAalert', 
+    'Hey @NancyPelosi @WhipHoyer: For an open internet, we need more privacy, but CISPA gives us less. https://eff.org/CISPA #CISPAalert'
+  ];
+
+  function random_tweet() {
+    var rand_rep_name = Math.floor(Math.random()*committee.length)
+      , rand_rep_tweet = Math.floor(Math.random()*rep_tweets.length)
+      , rand_leader_tweet = Math.floor(Math.random()*leader_tweets.length);
+
+    var rep_tweet = rep_tweets[rand_rep_tweet].replace('[[twitter_name]]', committee[rand_rep_name])
+      , leader_tweet = leader_tweets[rand_leader_tweet];
+
+    $('#tweet-rep').html(rep_tweet).attr('href', 'https://twitter.com/?status='+encodeURIComponent(rep_tweet));
+    $('#tweet-leader').html(leader_tweet).attr('href', 'https://twitter.com/?status='+encodeURIComponent(leader_tweet));
+
+  }
+  $('#suggest').click(random_tweet);
+  random_tweet();
+
+  // twitter badge opt-in
+  $('#twitter-opt-in').click(function(){
+    $('#twitter-badge').html('<iframe src="badge.html"></iframe>');
+  });
+
+  // embed
+  function embed_code(width, height) {
+    $('#embed pre').html('&lt;iframe src="https://cyberspying.eff.org/embed/2013-cispa/" style="border: 0; width: '+width+'px; height: '+height+'px;"&gt;&lt;/iframe&gt;');
+  }
+  $('.embed-size').click(function(){
+    $('.embed-size').removeClass('active');
+    $(this).addClass('active');
+    var size = $(this).attr('id').split('-')[1].split('x');
+    embed_code(size[0], size[1]);
+  });
+  $('#embed-500x500').click();
+
+  // next step
+  // if coming from action, next step is EFF donate
+  if(document.location.hash == '#action') {
+    $('#next-step').html('<p>Done tweeting? <a href="https://supporters.eff.org/donate/thanks-opposing-cispa">Defend privacy by supporting EFF</a>.<br/><a href="https://www.facebook.com/share.php?u=https://action.eff.org/o/9042/p/dia/action/public/?action_KEY=9048" target="_blank">Share on Facebook</a>.</p>');
+  }
+  // otherwise next step is action
+  else {
+    $('#next-step').html('<p>Done tweeting? <a href="https://action.eff.org/o/9042/p/dia/action/public/?action_KEY=9048">Tell Congress to defend privacy</a>.<br/>Not in the United States? <a href="https://action.eff.org/o/9042/p/dia/action/public/?action_KEY=9137">Take action here instead</a>.</p>');
+  }
+
+});
